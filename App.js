4a705458de9b063ba592838b562f0f955c8efdbe
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-08-17 20:50:40 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import "tailwindcss/dist/base.css";
+import "styles/globalStyles.css";
+import React from "react";
+import { css } from "styled-components/macro"; //eslint-disable-line
+
+
+import ComponentRenderer from "ComponentRenderer.js";
+import MainLandingPage from "MainLandingPage.js";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export default function App() {
+  // return <AnimationRevealPage disabled></AnimationRevealPage>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/components/:type/:subtype/:name">
+          <ComponentRenderer />
+        </Route>
+        <Route path="/components/:type/:name">
+          <ComponentRenderer />
+        </Route>
+        <Route path="/">
+          <MainLandingPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
